a9ebde64e0d2736710599b137c68988247bd96e7
hwang <ilyoung6016@gmail.com>
2023-03-21 16:24:23 +0900

    add description

--- prototype/src/Main.java
+++ prototype/src/Main.java
@@ -7,16 +7,19 @@ import product.Point;
 public class Main {
     public static void main(String[] args) {
 
+        /* create */
         Manager manager = new Manager();
         Line redLine = new Line("red", new Point(10, 10), new Point(5, 5));
         Line blueLine = new Line("blue", new Point(10, 10), new Point(5, 5));
         manager.register("redLine", redLine);
         manager.register("blueLine", blueLine);
 
+        /* clone */
         Shape cloneRedLine = manager.create("redLine");
         cloneRedLine.draw();
         Shape cloneBlueLine = manager.create("blueLine");
         cloneBlueLine.draw();
+
 
     }
 }
